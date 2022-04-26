efef301dd7dad796b501b8e776d1df11d0d9c210
ankitbarela <ankitvish1306@gmail.com>
2022-04-26 20:46:13 +0530

    Added the login

--- JokesWebApp/Controllers/JokesController.cs
+++ JokesWebApp/Controllers/JokesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using JokesWebApp.Data;
 using JokesWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JokesWebApp.Controllers
 {
@@ -55,7 +56,7 @@ namespace JokesWebApp.Controllers
 
             return View(joke);
         }
-
+        [Authorize]
         // GET: Jokes/Create
         public IActionResult Create()
         {
@@ -65,6 +66,7 @@ namespace JokesWebApp.Controllers
         // POST: Jokes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -97,6 +99,8 @@ namespace JokesWebApp.Controllers
         // POST: Jokes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -129,6 +133,7 @@ namespace JokesWebApp.Controllers
             return View(joke);
         }
 
+        [Authorize]
         // GET: Jokes/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
